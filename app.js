966d245ebe6e79aa63e56287826d57d831fb075b
darko <darko.milosevic@mmscode.com>
2019-06-21 15:05:40 +0200

    add and remove notes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs')
+const notes = require('./notes.js')
 
 yargs.command({
     command: 'add',
@@ -16,16 +17,22 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title:', argv.title)
-        console.log('Body:', argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
 yargs.command({
     command: 'remove',
     describe: 'Removing a note',
-    handler: function() {
-        console.log('Removing a note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title)
     }
 })
 
